86a4161d81518d68098e9ed741d91f2972816f7f
Wen Ting Wang <wenting@madetech.com>
2020-09-08 13:56:14 +0100

    Add policy for RDS monitoring role
    Or fix the monitoring level being reset (or not being set to 60).

--- modules/grafana/policies.tf
+++ modules/grafana/policies.tf
@@ -16,3 +16,12 @@ resource "aws_iam_role" "rds_monitoring_role" {
   name = "${var.prefix}-rds-monitoring-role"
   assume_role_policy = file("${path.module}/policies/rds_monitoring_role.json")
 }
+
+data "aws_iam_policy" "rds_monitoring_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+resource "aws_iam_role_policy_attachment" "rds_monitoring_policy_attachment" {
+  role       = "${aws_iam_role.rds_monitoring_role.name}"
+  policy_arn = "${data.aws_iam_policy.rds_monitoring_policy.arn}"
+}
