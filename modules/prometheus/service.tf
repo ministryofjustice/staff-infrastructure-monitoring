9e21acb698be7c2cdd4d3f8c8ff577a9c2efeba1
David Capper <david.capper@madetech.com>
2020-10-05 15:54:16 +0100

    Added thanos store container to prometheus service
    Co-authored-by: David Capper <chubberlisk@users.noreply.github.com>
    Co-authored-by: El <elena-vi@users.noreply.github.com>

--- modules/prometheus/service.tf
+++ modules/prometheus/service.tf
@@ -73,6 +73,32 @@ resource "aws_ecs_task_definition" "prometheus_task_definition" {
         "awslogs-group" : "${aws_cloudwatch_log_group.prometheus_cloudwatch_log_group.name}"
       }
     }
+  },
+  {
+    "name": "thanos-store",
+    "cpu": ${var.fargate_cpu},
+    "memory": ${var.fargate_memory},
+    "image": "quay.io/thanos/thanos:v0.15.0",
+    "essential": false,
+    "command": [
+      "store",
+      "--grpc-address=0.0.0.0:20091",
+      "--http-address=0.0.0.0:20902",
+      "--data-dir=/tmp/thanos/store",
+      "--objstore.config=${data.template_file.storage_config.rendered}"
+    ],
+    "portMappings": [{
+      "hostPort": 20902,
+      "containerPort": 20902
+    }],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-region" : "${var.aws_region}",
+        "awslogs-stream-prefix": "${var.prefix}-thqr",
+        "awslogs-group" : "${aws_cloudwatch_log_group.prometheus_cloudwatch_log_group.name}"
+      }
+    }
   }]
   DEFINITION
 }
@@ -104,6 +130,22 @@ resource "aws_ecs_service" "prometheus_ecs_service" {
 resource "aws_cloudwatch_log_group" "prometheus_cloudwatch_log_group" {
   name              = "${var.prefix}-prometheus-cloudwatch-log-group"
   retention_in_days = 7
+
+  tags = var.tags
+}
+
+data "template_file" "storage_config" {
+  template = file("${path.module}/s3config.template.yml")
+
+  vars = {
+    bucket_name = aws_s3_bucket.storage.bucket
+    endpoint = "s3.eu-west-2.amazonaws.com"
+  }
+}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "${var.prefix}-thanos-storage"
+  acl    = "private"
 
   tags = var.tags
 }
