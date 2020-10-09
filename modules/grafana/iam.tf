6d91a6a3384c50aff75166f024d5db1cedb29467
Wen Ting Wang <wenting@madetech.com>
2020-10-09 11:11:23 +0100

    Add image rendering and external image storage for Grafana

--- modules/grafana/iam.tf
+++ modules/grafana/iam.tf
@@ -9,3 +9,22 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_read_access_policy_attachm
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
   role       = aws_iam_role.task_role.name
 }
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name = "${var.prefix}-grafana-s3-access"
+
+  policy = data.template_file.s3_access_policy.rendered
+}
+
+data "template_file" "s3_access_policy" {
+  template = file("${path.module}/policies/s3_access_policy.template.json")
+
+  vars = {
+    bucket = aws_s3_bucket.external_image_storage.bucket
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_policy_attachment" {
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+  role       = aws_iam_role.task_role.name
+}
