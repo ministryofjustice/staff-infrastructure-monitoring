87c100929993302a76f0710590ca3c0be17e8cbb
Elena <elena@madetech.com>
2020-10-09 10:26:23 +0100

    enable serverside encryption by adding kms and turning it on s3 bucket

--- modules/prometheus/service.tf
+++ modules/prometheus/service.tf
@@ -150,4 +150,17 @@ resource "aws_s3_bucket" "storage" {
   acl    = "private"
 
   tags = var.tags
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.storage_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_kms_key" "storage_key" {
+  description             = "${var.prefix}-thanos-storage encryption key"
 }
