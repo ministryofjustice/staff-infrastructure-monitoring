a2b406696832ebae4fb4bed31ed5d4a2f8d0d42d
Wen Ting Wang <wenting@madetech.com>
2020-10-12 18:21:00 +0100

    Fix Terraform interpolation warning in Prometheus service file

--- modules/prometheus/service.tf
+++ modules/prometheus/service.tf
@@ -169,7 +169,7 @@ resource "aws_s3_bucket" "storage" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.storage_key.arn}"
+        kms_master_key_id = aws_kms_key.storage_key.arn
         sse_algorithm     = "aws:kms"
       }
     }
