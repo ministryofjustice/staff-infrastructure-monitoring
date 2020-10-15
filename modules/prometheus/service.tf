a6e4050bb6527e2d585fc9f31c7177423b2a4580
Wen Ting Wang <wenting@madetech.com>
2020-10-15 11:34:39 +0100

    Add S3 bucket metric config for Prometheus long-term storage

--- modules/prometheus/service.tf
+++ modules/prometheus/service.tf
@@ -159,23 +159,3 @@ data "template_file" "storage_config" {
     kms_key_id = aws_kms_key.storage_key.key_id
   }
 }
-
-resource "aws_s3_bucket" "storage" {
-  bucket = "${var.prefix}-thanos-storage"
-  acl    = "private"
-
-  tags = var.tags
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.storage_key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
-
-resource "aws_kms_key" "storage_key" {
-  description             = "${var.prefix}-thanos-storage encryption key"
-}
