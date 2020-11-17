13f218bd9c97deb689e43be71455a1e299d42df9
David Capper <david.capper@madetech.com>
2020-11-17 08:25:38 +0000

    Add cloudwatch alarm that monitors healthy grafana hosts

---
+++ modules/grafana/alarm.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_metric_alarm" "grafana" {
+  alarm_name                = "Monitoring and Alerting platform"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "1"
+  alarm_description         = "Number of healthy Grafana hosts"
+
+  dimensions = {
+    LoadBalancer = aws_alb.main_grafana.arn_suffix
+    TargetGroup  = aws_alb_target_group.app_grafana.arn_suffix
+  }
+
+  insufficient_data_actions = []
+}
