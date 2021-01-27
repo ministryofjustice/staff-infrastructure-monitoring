66a469ba2099915cd3484f4acfcfe98c0a0dde10
Daniel Wilson <dan@00wilson.co.uk>
2021-01-27 12:29:17 +0000

    more work on the script, now creates a user successfully

--- scripts/create_ci_user.rb
+++ scripts/create_ci_user.rb
@@ -4,20 +4,38 @@
 require 'net/http'
 require 'json'
 
-# curl -u pttp:Puu10rviYsBQSSsg0J7usr29Gs9Kn5LlEg3EYQDebvc= 
-# / https://monitoring-alerting.dev.staff.service.justice.gov.uk/api/admin/users 
-# / --data-binary '{"name": "test-user", "email": "emma_lc_123@hotmail.com", "login": "test", "password": "test123"}'
-# / -H "Content-type: application/json"
+grafana_url = "https://monitoring-alerting.dev.staff.service.justice.gov.uk/api"
+username = "test"
 
-def create_ci_user
-  grafana_url = "https://monitoring-alerting.dev.staff.service.justice.gov.uk/api/admin/users"
+def authenticate_request(req)
   grafana_admin_username = "pttp"
   grafana_admin_password = "Puu10rviYsBQSSsg0J7usr29Gs9Kn5LlEg3EYQDebvc="
 
-  uri = URI(grafana_url)
-  req = Net::HTTP::Post.new(uri)
   req.basic_auth grafana_admin_username, grafana_admin_password
+end
 
+def make_request(req,uri)
+  Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
+    http.request(req)
+  }
+end
+
+def check_for_user(grafana_url,username)
+  uri = URI("#{grafana_url}/users/lookup?loginOrEmail=#{username}")
+
+  req = Net::HTTP::Get.new(uri)
+  authenticate_request(req)
+
+  res = make_request(req,uri)
+
+  res.code != "404"
+end
+
+def create_ci_user(grafana_url)
+  uri = URI("#{grafana_url}/admin/users")
+  req = Net::HTTP::Post.new(uri)
+  authenticate_request(req)
+  
   req.set_form_data(
     'name' => 'test-user',
     'email' => 'emma_lc_123@hotmail.com',
@@ -25,9 +43,7 @@ def create_ci_user
     'password' => 'test123'
   )
 
-  res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-    http.request(req)
-  }
+  res = make_request(req,uri)
 
   case res
   when Net::HTTPSuccess, Net::HTTPRedirection
@@ -38,25 +54,16 @@ def create_ci_user
   end
 end
 
-user_id = create_ci_user()
-puts user_id
-
-def assign_admin_permissions_to_ci_user(user_id)
-    grafana_url = "https://monitoring-alerting.dev.staff.service.justice.gov.uk/api/admin/users/#{user_id}/permissions"
-    grafana_admin_username = "pttp"
-    grafana_admin_password = "Puu10rviYsBQSSsg0J7usr29Gs9Kn5LlEg3EYQDebvc="
-
-    uri = URI(grafana_url)
-    req = Net::HTTP::Put.new(uri)
-    req.basic_auth grafana_admin_username, grafana_admin_password
+def assign_admin_permissions_to_ci_user(grafana_url, user_id)
+    uri = URI("#{grafana_url}/org/users/#{user_id}")
+    req = Net::HTTP::Patch.new(uri)
+    authenticate_request(req)
   
     req.set_form_data(
-      'isGrafanaAdmin' => true
+      'role' => 'Admin'
     )
   
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-      http.request(req)
-    }
+    res = make_request(req,uri)
 
     case res
     when Net::HTTPSuccess, Net::HTTPRedirection
@@ -67,6 +74,9 @@ def assign_admin_permissions_to_ci_user(user_id)
     end
 end
 
-assign_admin_permissions_to_ci_user(user_id)
 
-  
+# user_id = create_ci_user(grafana_url)
+# puts user_id
+# assign_admin_permissions_to_ci_user(grafana_url, user_id)
+
+puts check_for_user(grafana_url,username)
