552e3f8baeb2fc291c146fa938ffde69c0f2922f
Daniel Wilson <68431297+wanieldilson@users.noreply.github.com>
2021-02-15 10:33:30 +0000

    Revert "Cidr block change" (#20)
    * Revert "Cidr block change (#19)"
    
    This reverts commit f2b65451fc83dc1d640803c95c35e5a579c83578.
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,69 +1,50 @@
 ####################################### Hostnames #################################################
 
 output "grafana_hostname" {
-  value = {
-    old = module.grafana_old.domain
-    new = module.grafana.domain
-  }
+  value = module.grafana.domain
 }
 
 output "prometheus_hostname" {
-  value = {
-    old = module.prometheus_old.hostname
-    new = module.prometheus.hostname
-  }
+  value = module.prometheus.hostname
 }
 
 output "snmp_exporter_hostname" {
-  value = {
-    old = module.snmp_exporter_old.hostname
-    new = module.snmp_exporter.hostname
-  }
+  value = module.snmp_exporter.hostname
 }
 
 output "blackbox_exporter_hostname" {
-  value = {
-    old = module.blackbox_exporter_old.hostname
-    new = module.blackbox_exporter.hostname
-  }
+  value = module.blackbox_exporter.hostname
 }
 
 ####################################### Repositories #################################################
 
 output "prometheus_repository" {
   value = {
-    old_registry_id    = module.prometheus_old.repository.registry_id
-    old_repository_url = module.prometheus_old.repository.repository_url
-    registry_id        = module.prometheus.repository.registry_id
-    repository_url     = module.prometheus.repository.repository_url
+    registry_id    = module.prometheus.repository.registry_id
+    repository_url = module.prometheus.repository.repository_url
   }
 }
 
 output "snmp_exporter_repository" {
   value = {
-    old_registry_id    = module.snmp_exporter_old.repository.registry_id
-    old_repository_url = module.snmp_exporter_old.repository.repository_url
-    registry_id        = module.snmp_exporter.repository.registry_id
-    repository_url     = module.snmp_exporter.repository.repository_url
+    registry_id    = module.snmp_exporter.repository.registry_id
+    repository_url = module.snmp_exporter.repository.repository_url
   }
 }
 
 output "blackbox_exporter_repository" {
   value = {
-    old_registry_id    = module.blackbox_exporter_old.repository.registry_id
-    old_repository_url = module.blackbox_exporter_old.repository.repository_url
-    registry_id        = module.blackbox_exporter.repository.registry_id
-    repository_url     = module.blackbox_exporter.repository.repository_url
+    registry_id    = module.blackbox_exporter.repository.registry_id
+    repository_url = module.blackbox_exporter.repository.repository_url
   }
 }
 
 output "grafana_database_config" {
   value = {
-    old_grafana_db_subnet_group_name    = module.grafana_old.grafana_db_subnet_group_name
-    old_grafana_db_in_security_group_id = module.grafana_old.db_in_security_group_id
-    old_rds_monitoring_role_arn         = module.monitoring_platform_old.rds_monitoring_role_arn
-    grafana_db_subnet_group_name        = module.grafana.grafana_db_subnet_group_name
-    grafana_db_in_security_group_id     = module.grafana.db_in_security_group_id
-    rds_monitoring_role_arn             = module.monitoring_platform.rds_monitoring_role_arn
+    grafana_db_subnet_group_name    = module.grafana.grafana_db_subnet_group_name
+    grafana_db_in_security_group_id = module.grafana.db_in_security_group_id
+    rds_monitoring_role_arn         = module.monitoring_platform.rds_monitoring_role_arn
+
   }
 }
+
