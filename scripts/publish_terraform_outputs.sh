e7a0b5c36cec67310fbc402acd90f85f3d90f2de
andycohen <andy.cohen@hybridtechsolutions.co.uk>
2021-02-26 12:36:34 +0000

    Add script to populate SSM from Terraform Outputs

---
+++ scripts/publish_terraform_outputs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+git_repo_name=$(basename `git rev-parse --show-toplevel`)
+terraform_outputs=$(terraform output -json terraform_outputs)
+
+aws ssm put-parameter --name "/terraform_$git_repo_name/$ENV/outputs" \
+  --description "Terraform outputs that other pipelines or processes depend on" \
+  --value "$terraform_outputs" \
+  --type String \
+  --overwrite
