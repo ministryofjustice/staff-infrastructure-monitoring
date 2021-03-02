7c447e67feec2dc6074ae5cefd4a501d56a77b37
andycohen <andy.cohen@hybridtechsolutions.co.uk>
2021-03-02 11:13:36 +0000

    Fix repo name logic

--- scripts/publish_terraform_outputs.sh
+++ scripts/publish_terraform_outputs.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-git_dir=$(git rev-parse --show-toplevel)
-git_repo_name=$(basename $git_dir)
 terraform_outputs=$(terraform output -json)
 
-aws ssm put-parameter --name "/terraform_$git_repo_name/$ENV/outputs" \
+aws ssm put-parameter --name "/terraform_staff_infrastructure_monitoring/$ENV/outputs" \
   --description "Terraform outputs that other pipelines or processes depend on" \
   --value "$terraform_outputs" \
   --type String \
