1dd5a395ad9b178a93b174fdf31944fe81da75c2
elcorbs <emma@madetech.com>
2021-03-02 11:48:31 +0000

    pull selenium image for aws ecr repository in shared services account ID
    Changed run_test script to grep from the beginning of the line, as I often have
    dev & my workspace vars, with one commented out

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-domain="`grep -m 1 'vpn_hosted_zone_domain' ../terraform.tfvars`"
-prefix="`grep -m 1 'domain_prefix' ../terraform.tfvars`"
-username="`grep -m 1 'grafana_admin_username' ../terraform.tfvars`"
-password="`grep -m 1 'grafana_admin_password' ../terraform.tfvars`"
+domain="`grep -m 1 '^vpn_hosted_zone_domain' ../terraform.tfvars`"
+prefix="`grep -m 1 '^domain_prefix' ../terraform.tfvars`"
+username="`grep -m 1 '^grafana_admin_username' ../terraform.tfvars`"
+password="`grep -m 1 '^grafana_admin_password' ../terraform.tfvars`"
 
 echo "Running rspec"
 
