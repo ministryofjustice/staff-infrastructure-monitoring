bfb05d180ecf06b880c55e6a3f0c7cc61673690b
andycohen <andy.cohen@hybridtechsolutions.co.uk>
2021-03-02 10:22:18 +0000

    Revert to type String

--- scripts/publish_terraform_outputs.sh
+++ scripts/publish_terraform_outputs.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 git_repo_name=$(basename `git rev-parse --show-toplevel`)
 terraform_outputs=$(terraform output -json)
 
+echo $git_repo_name
+
 aws ssm put-parameter --name "/terraform_$git_repo_name/$ENV/outputs" \
   --description "Terraform outputs that other pipelines or processes depend on" \
   --value "$terraform_outputs" \
-  --type SecureString \
+  --type String \
   --overwrite
