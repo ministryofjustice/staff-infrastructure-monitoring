2f4c4785afabe53d6be8313796d90fa57580a794
elcorbs <emma@madetech.com>
2021-03-18 12:19:29 +0000

    Test deploying to kubernetes in the pipeline

--- scripts/deploy_kubernetes.sh
+++ scripts/deploy_kubernetes.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+
+set -x
+
 export KUBECONFIG="./kubernetes/kubeconfig"
 
-cluster_role_arn=$(GET THIS FROM TERRAFROM OUTPUTS) # THE ROLE ARN CURRENTLY BEING OUTPUT BY THE TERRAFORM ISNT THE CORRECT ONE
-prometheus_image_repo=$(GET THIS FROM TF OUTPUTS - prometheus_repository_v2.repository_url)
+TEMP_ROLE=`aws-vault exec moj-shared-services -- aws sts assume-role --role-arn $TF_VAR_assume_role --role-session-name ci-authenticate-kubernetes-782`
+
+access_key=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+secret_access_key=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+session_token=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+
+cluster_role_arn=$(terraform output eks_cluster_worker_iam_role_arn)
+prometheus_image_repo=$(terraform output -json prometheus_repository_v2 | jq  '.repository_url' | sed 's/"//g')
+cluster_name=$(terraform output eks_cluster_id)
+
+echo $(AWS_ACCESS_KEY_ID=$access_key AWS_SECRET_ACCESS_KEY=$secret_access_key AWS_SESSION_TOKEN=$session_token aws  sts get-caller-identity)
 
 # SAVE KUBECONFIG FILE
-
-aws eks --region eu-west-2 update-kubeconfig --name mojo-$ENV-ima-cluster #MAYBE GET THE CLUTER NAME FROM TERRAFORM OUTPUTS AS WELL
+AWS_ACCESS_KEY_ID=$access_key AWS_SECRET_ACCESS_KEY=$secret_access_key AWS_SESSION_TOKEN=$session_token aws eks --region eu-west-2 update-kubeconfig --name $cluster_name --role-arn $TF_VAR_assume_role
 
 # UPDATE CONFIGMAP
 helm upgrade --install mojo-$ENV-ima-configmap ./kubernetes/auth-configmap --set rolearn=$cluster_role_arn
