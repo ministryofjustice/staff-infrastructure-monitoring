ab7794dc13709f5c3907f22cc4dfe0779bf87bf8
Andy Cohen <andrew.cohen1@justice.gov.uk>
2021-04-23 10:55:12 +0100

    Fix S3 ARN output

--- modules/s3_bucket/outputs.tf
+++ modules/s3_bucket/outputs.tf
@@ -1,5 +1,5 @@
 output "bucket_arn" {
-  value = var.encryption_enabled ? concat(aws_s3_bucket.encrypted.*.bucket, [""])[0] : concat(aws_s3_bucket.non-encrypted.*.bucket, [""])[0]
+  value = var.encryption_enabled ? concat(aws_s3_bucket.encrypted.*.arn, [""])[0] : concat(aws_s3_bucket.non-encrypted.*.arn, [""])[0]
 }
 
 output "bucket_name" {
