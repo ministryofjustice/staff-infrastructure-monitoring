9e5f45ad614a6a3298f06287bdd6ae1577820c08
Andy Mitchell <me@themitchell.co.uk>
2021-04-28 15:21:29 +0100

    Publish terraform outputs in dev environments post apply

--- scripts/publish_terraform_outputs.sh
+++ scripts/publish_terraform_outputs.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 terraform_outputs=$(terraform output -json)
+env=$(echo $terraform_outputs | jq '.env.value' | sed 's/"//g')
 
-aws ssm put-parameter --name "/terraform_staff_infrastructure_monitoring/$ENV/outputs" \
+aws ssm put-parameter --name "/terraform_staff_infrastructure_monitoring/$env/outputs" \
   --description "Terraform outputs that other pipelines or processes depend on" \
   --value "$terraform_outputs" \
   --type String \
