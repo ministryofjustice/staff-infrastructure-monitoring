248fa7b2156095566ff9a8258283f15baa9af1f6
Dan Wilson <daniel.wilson4@justice.gov.uk>
2022-01-06 11:02:44 +0000

    :bookmark: add tags to blackbox module

--- modules/blackbox_exporter/repository.tf
+++ modules/blackbox_exporter/repository.tf
@@ -5,7 +5,7 @@ resource "aws_ecr_repository" "blackbox_exporter" {
   image_scanning_configuration {
     scan_on_push = true
   }
-
+  tags = var.tags
 }
 
 resource "aws_ecr_repository_policy" "blackbox_exporter_ecr_policy" {
