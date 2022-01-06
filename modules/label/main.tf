77954042962bf22f93b34b266510c1194b32e6a6
Dan Wilson <daniel.wilson4@justice.gov.uk>
2022-01-06 10:19:55 +0000

    :bookmark: correct all tags to new standards

--- modules/label/main.tf
+++ modules/label/main.tf
@@ -8,11 +8,11 @@ module "label" {
   stage     = terraform.workspace
 
   tags = {
-    "business-unit"    = "MoJO"
+    "business-unit"    = "HQ"
     "environment-name" = "global"
     "owner"            = var.owner-email
     "is-production"    = var.is-production
-    "application"      = "Infrastructure Monitoring and Alerting"
+    "application"      = "infrastructure-monitoring"
     "source-code"      = "https://github.com/ministryofjustice/staff-infrastructure-monitoring"
     "status"           = var.label_status
     "notes"            = var.label_notes
