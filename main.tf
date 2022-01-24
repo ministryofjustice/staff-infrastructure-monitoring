884d05fa44c37def1a11aba5cf20a7ec6a16e15c
Gary Henderson <26419401+Gary-H9@users.noreply.github.com>
2022-01-24 15:44:16 +0000

    :wrench: Add vpc_subnet_cidr_blocks to snmp_exporter

--- main.tf
+++ main.tf
@@ -190,6 +190,7 @@ module "snmp_exporter_v2" {
   cluster_id         = module.monitoring_platform_v2.cluster_id
   public_subnet_ids  = module.monitoring_platform_v2.public_subnet_ids
   private_subnet_ids = module.monitoring_platform_v2.private_subnet_ids
+  vpc_subnet_cidr_blocks = concat(module.monitoring_platform_v2.private_subnet_cidr_blocks, module.monitoring_platform_v2.public_subnet_cidr_blocks)
 
   execution_role_arn = module.monitoring_platform_v2.execution_role_arn
 
