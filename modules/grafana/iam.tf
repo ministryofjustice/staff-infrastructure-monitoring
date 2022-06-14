d1b7a8907f9122a11dee768ef0b367a81eea942f
Daniel Wilson <68431297+wanieldilson@users.noreply.github.com>
2022-06-14 12:04:59 +0100

    :passport_control: New user for Cloudwatch access permissions (#217)
    :passport_control: New user for cloudwatch access permissions

--- modules/grafana/iam.tf
+++ modules/grafana/iam.tf
@@ -22,3 +22,16 @@ resource "aws_iam_role_policy_attachment" "s3_access_policy_attachment" {
   policy_arn = aws_iam_policy.s3_access_policy.arn
   role       = aws_iam_role.task_role.name
 }
+
+resource "aws_iam_user" "cloudwatch_datasource_user" {
+  name = "${var.prefix_pttp}-cloudwatch-datasource-user"
+
+  tags = var.tags
+}
+
+resource "aws_iam_user_policy" "cloudwatch_datasource_access_policy" {
+  policy = file("${path.module}/policies/cloudwatch_metrics_and_logs.json")
+  user   = aws_iam_user.cloudwatch_datasource_user.name
+}
+
+
