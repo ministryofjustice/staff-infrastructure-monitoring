d660487d3d355c6d82f599027cbb28d5267642f0
Dan Wilson <daniel.wilson4@justice.gov.uk>
2022-08-03 14:09:04 +0100

    :robot: automate pulling the tfvars down from aws

---
+++ scripts/generate_tfvars.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+get_outputs() {
+  printf "\nGenerating tfvars file for $ENV\n\n"
+
+  tfvars=`aws ssm get-parameter --with-decryption --name /staff-infrastructure-monitoring/terraform.tfvars | jq -r .Parameter.Value`
+}
+
+get_outputs
+
+cat <<EOF > terraform.tfvars
+${tfvars}
+EOF
+
+echo "Take a look at your terraform.tfvars file and replace <YOUR-WORKSPACE-NAME> placeholder with... Well I'm sure you can guess!"
+read -n 1 -r -s -p $'Press enter to continue...\n'
